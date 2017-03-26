9fcc3a65b585445f26b395e619cdcb1d25597ded
Lech Wojas <lwojas@hotmail.com>
2017-03-27 00:26:02 +0200

    Updated portfolio page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-	alert("The page has loaded");
+	//alert("The page has loaded");
 	$(".content-box").hide();
 	$(".image-box").hide();
 	$(".assignment1").hide();
@@ -7,6 +7,7 @@ $(document).ready( function () {
 		$(".image-box").fadeIn(500);
 	});
 	var LoopScroll = true;
+	var one = $('#one');
 	floatAnimation();
 	
 	$(window).scroll(function(event){
@@ -14,53 +15,48 @@ $(document).ready( function () {
 			LoopScroll = false;
 			var lastPosition = $('.plane').css("transform");
 			//console.log(lastPosition);
-			$('.plane').stop(true);
+			$('.plane').stop();
 			$('.plane').transition({transform: 'rotateX(-5deg) rotateY(5deg)'}, 800, function(){
 				//console.log("The scroll callback has fired");
-				$('.plane').transition({transform: lastPosition}, 800);
-				LoopScroll = true;
+				$('.plane').transition({transform: lastPosition}, 800, function() {
+					LoopScroll = true;
+				});
 			});
 		};
    	});
 
 	function floatAnimation() {
 		$('.plane').transition({transform: 'rotateY(5deg) translateZ(10px)'}, 5000, function(){
-				floaterBack();
+			floaterBack();
 		});
 	};
 
 	function floaterBack() {
-			$('.plane').transition({transform: 'rotateY(10deg) translateZ(-10px)'}, 5000, function(){
-				floatAnimation();
-				console.log('This function has fired');
+		$('.plane').transition({transform: 'rotateY(10deg) translateZ(-10px)'}, 5000, function(){
+			floatAnimation();
+			console.log('This function has fired');
 		});
 	};
 
 
-	$('.portfolio-grid').hover( function() {
+	$('#one').hover( function() {
+		// glowAnimation.call(one);
 		$('.assignment1').stop(true, true);
-		$(".assignment1").show();
-		$('.assignment1').transition({width: '100%', opacity: '1', top: '-1000px', transform: 'translate3d(0px,-200px,500px)'}, 1000);
+		$('.assignment1').show();
+		$('.assignment1').transition({width: '100%', opacity: '1', top: '-1000px', transform: 'translate3d(0px,-200px,500px)'}, 500);
 	}, function() {
 		$('.assignment1').stop(true);
 		$('.assignment1').transition({width: '33%', opacity: '0', top: '0px', transform: 'translate3d(0px,0px,0px)'}, 500);
 	});
 
 
-/*	var lastScrollTop = 0;
-	$(window).scroll(function(event){
-   		var st = $(this).scrollTop();
-   		if (st > lastScrollTop){
-   			$('.plane').transition({transform: 'rotateX(-5deg) rotateY(10deg)'}, 800, function(){
-   				floaterBack();
-   			});
-       // downscroll code
-   		} else {
-      // upscroll code
-   		}
-   		lastScrollTop = st;
-	});*/
-
+	/*function glowAnimation() {
+		if (glowLoop === true) {
+			this.transition({textShadow: '-1px -2px 10px turquoise'}, 1000, false, function() {
+				this.transition({textShadow: '0px 0px 0px turquoise'}, 1000, false, glowAnimation);
+			});
+		};
+	};*/
 
 	
 	/*$('.plane').transition({transform: 'rotateY(10deg)'}, 2000, function(){
